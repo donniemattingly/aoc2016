614cd02f35c663e28bb39831d41240ebffb1c638
Donnie Mattingly <you@example.com>
2019-11-07 23:36:51 -0500

    part 2 solution

--- lib/day1.ex
+++ lib/day1.ex
@@ -21,59 +21,70 @@ defmodule Day1 do
     end)
   end
 
-  def part1 do
+  def part2 do
     move(get_processed_input(), :n, {0, 0}, %{})
   end
 
-  def update_visits(visits, pos) do
-    visits
-    |> Map.update(pos, 0, fn(val) ->
-      IO.puts("visited twice: #{elem(pos, 0)}, #{elem(pos, 1)}")
-      val + 1
+  def update_visits(visits, all_pos) do
+    all_pos |> Enum.reduce(visits, fn(pos, acc) ->
+      acc
+      |> Map.update(pos, 1, fn(val) ->
+        IO.puts("visited twice: #{elem(pos, 0)}, #{elem(pos, 1)}")
+        val + 1
+      end)
     end)
   end
 
   def move([], _, pos, visits) do
     visits
+    |> Enum.filter(fn({k, v}) -> v > 1 end)
   end
 
   def move([{:r, amount} | rest], :n, {x, y}, visits) do
     pos = {x + amount, y}
-    move(rest, :e, pos, visits |> update_visits(pos))
+    all_pos = 1..amount |> Enum.map(&({x + &1, y}))
+    move(rest, :e, pos, visits |> update_visits(all_pos))
   end
 
   def move([{:r, amount} | rest], :e, {x, y}, visits) do
     pos = {x, y - amount}
-    move(rest, :s, pos, visits |> update_visits(pos))
+    all_pos = 1..amount |> Enum.map(&({x, y - &1}))
+    move(rest, :s, pos, visits |> update_visits(all_pos))
   end
 
   def move([{:r, amount} | rest], :s, {x, y}, visits) do
     pos = {x - amount, y}
-    move(rest, :w, pos, visits |> update_visits(pos))
+    all_pos = 1..amount |> Enum.map(&({x - &1, y}))
+    move(rest, :w, pos, visits |> update_visits(all_pos))
   end
 
   def move([{:r, amount} | rest], :w, {x, y}, visits) do
     pos = {x, y + amount}
-    move(rest, :n, pos, visits |> update_visits(pos))
+    all_pos = 1..amount |> Enum.map(&({x, y + &1}))
+    move(rest, :n, pos, visits |> update_visits(all_pos))
   end
 
   def move([{:l, amount} | rest], :n, {x, y}, visits) do
     pos = {x - amount, y}
-    move(rest, :w, pos, visits |> update_visits(pos))
+    all_pos = 1..amount |> Enum.map(&({x - &1, y}))
+    move(rest, :w, pos, visits |> update_visits(all_pos))
   end
 
   def move([{:l, amount} | rest], :e, {x, y}, visits) do
     pos = {x, y + amount}
-    move(rest, :n, pos, visits |> update_visits(pos))
+    all_pos = 1..amount |> Enum.map(&({x, y + &1}))
+    move(rest, :n, pos, visits |> update_visits(all_pos))
   end
 
   def move([{:l, amount} | rest], :s, {x, y}, visits) do
     pos = {x + amount, y}
-    move(rest, :e, pos, visits |> update_visits(pos))
+    all_pos = 1..amount |> Enum.map(&({x + &1, y}))
+    move(rest, :e, pos, visits |> update_visits(all_pos))
   end
 
   def move([{:l, amount} | rest], :w, {x, y}, visits) do
     pos = {x, y - amount}
-    move(rest, :s, pos, visits |> update_visits(pos))
+    all_pos = 1..amount |> Enum.map(&({x , y - &1}))
+    move(rest, :s, pos, visits |> update_visits(all_pos))
   end
 end
