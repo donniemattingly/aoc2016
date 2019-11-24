d3c542ffe3f1146c8a4b7a131726aca104dd9381
Donnie Mattingly <donniewmattingly@gmail.com>
2019-11-24 15:04:25 -0500

    day 18

--- lib/day18.ex
+++ lib/day18.ex
@@ -1,10 +1,10 @@
 defmodule Day18 do
   def real_input do
-    ".^^^^^.^^^..^^^^^...^.^..^^^.^^....^.^...^^^...^^^^..^...^...^^.^.^.......^..^^...^.^.^^..^^^^^...^."
+    '.^^^^^.^^^..^^^^^...^.^..^^^.^^....^.^...^^^...^^^^..^...^...^^.^.^.......^..^^...^.^.^^..^^^^^...^.'
   end
 
   def sample_input do
-    "..^^."
+    '.^^.^.^^^^'
   end
 
   def sample_input2 do
@@ -15,13 +15,13 @@ defmodule Day18 do
   def sample do
     sample_input
     |> parse_input1
-    |> solve1
+    |> solve(10)
   end
 
   def part1 do
     real_input1
     |> parse_input1
-    |> solve1
+    |> solve(40)
   end
 
 
@@ -43,15 +43,65 @@ defmodule Day18 do
   def parse_input1(input), do: parse_input(input)
   def parse_input2(input), do: parse_input(input)
 
-  def solve1(input), do: solve(input)
-  def solve2(input), do: solve(input)
+  def solve1(input), do: solve(input, 40)
+  def solve2(input), do: solve(input, 400000)
 
   def parse_input(input) do
     input
   end
 
-  def solve(input) do
+  def solve(input, size) do
     input
+    |> get_room(size)
+    |> count_safe
+  end
+
+  def render_room(room) do
+    room
+    |> Enum.reverse
+    |> Enum.join("\n")
+    |> IO.puts
+  end
+
+  def count_safe(room) do
+    room
+    |> List.flatten
+    |> Enum.count(fn tile -> tile == ?. end)
+  end
+
+  def get_room(start_line, num_rows) do
+    do_get_room([start_line], num_rows)
+  end
+
+  defp do_get_room(rows, 1), do: rows
+
+  defp do_get_room([cur_row | rest] = rows, count) do
+    do_get_room([next_line(cur_row) | rows], count - 1)
+  end
+
+  def next_line(line) do
+    '.'++line++'.'
+    |> Enum.chunk_every(3, 1)
+    |> Enum.filter(fn x -> length(x) == 3 end)
+    |> Enum.map(&get_next_tile_from_chunk/1)
+  end
+
+  def get_next_tile_from_chunk(chunk) do
+    case chunk |> chunk_to_tiles |> is_trap? do
+      true -> ?^
+      false -> ?.
+    end
+  end
+
+  def chunk_to_tiles(chunk) do
+    chunk
+    |> Enum.map(fn x ->
+      case x do
+        ?. -> false
+        ?^ -> true
+      end
+    end)
+    |> List.to_tuple
   end
 
   def is_trap?(tiles) do
